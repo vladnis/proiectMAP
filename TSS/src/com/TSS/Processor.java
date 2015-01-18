fe878774947d314ff60739022396668849d94507
eugen01 <eugen.a.ionescu@gmail.com>
2015-01-18 15:53:16 +0200

    Added Scheduler skeleton

---
+++ TSS/src/com/TSS/Processor.java
@@ -0,0 +1,18 @@
+package com.TSS;
+
+import java.lang.Thread;
+
+public class Processor extends Thread {
+
+	private Scheduler scheduler;
+	private Node task;
+	
+	public Processor(Scheduler sched){
+	}
+	
+	public void run(){
+		
+		scheduler.get
+		
+	}
+}
--- TSS/src/com/TSS/Scheduler.java
+++ TSS/src/com/TSS/Scheduler.java
@@ -1,5 +1,33 @@
 package com.TSS;
 
+import java.util.List;
+
 public class Scheduler {
 
+	public Graph graph;
+	
+	public List<Processor> processorList;
+	
+	public scheduler(Graph g){
+	
+		this.graph = g;
+	}
+	
+	
+	public Node getTask(int procID){
+		
+		Node task;
+		
+		if (graph.getNrTasks() == 0){
+			System.out.println("All tasks completed; Shutting Down");
+			notifyAll();
+		}
+		while ( task = graph.findUnbindedTask(procID) == null);
+		
+		return task;
+	}
+	
+	public void removeTask(int taskID){
+		graph.deleteNode(taskID);
+	}
 }
