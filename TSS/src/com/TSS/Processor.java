63fad3f183015372c80b927333ed3d46fcb2c256
eugen01 <eugen.a.ionescu@gmail.com>
2015-01-18 16:01:53 +0200

    Added Processor class

--- TSS/src/com/TSS/Processor.java
+++ TSS/src/com/TSS/Processor.java
@@ -7,12 +7,27 @@ public class Processor extends Thread {
 	private Scheduler scheduler;
 	private Node task;
 	
+	public int procID;
+	
 	public Processor(Scheduler sched){
 	}
 	
 	public void run(){
 		
-		scheduler.get
+		this.task = scheduler.getTask(this.procID);
 		
+		try{
+			task.execute();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		System.out.println("Processor "+procID+" finished task "+task.getId());
+		scheduler.removeTask(task.getId());
+	}
+	
+	public int getID(){
+		
+		return this.procID;
 	}
 }
